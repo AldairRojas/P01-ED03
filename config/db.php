a34ed8c011f1ae1bc1e643813fc31fca939f86e1
AldairRojas <rfloresaldairal@uss.edu.pe>
2024-10-30 22:58:11 -0500

    actualizacion

--- config/db.php
+++ config/db.php
@@ -1,14 +1,14 @@
 <?php
-// Datos de conexión a la base de datos
+
 $host = "localhost";
-$dbname = "denuncias_db"; // Nombre de la base de datos
-$username = "root"; // Usuario por defecto en XAMPP
-$password = ""; // Contraseña por defecto en XAMPP es vacía
+$dbname = "denuncias_db"; 
+$username = "root"; 
+$password = ""; 
 
 try {
-    // Crear la conexión usando PDO
+   
     $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
-    // Configurar el manejo de errores para que lance excepciones
+   
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     
 } catch (PDOException $e) {
